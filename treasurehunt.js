c10fd70c6ff104082bdaf791e1008abf44352ae7
Oliver Cass <olicass99@gmail.com>
2020-12-09 10:18:01 +0000

    video test

--- treasurehunt.js
+++ treasurehunt.js
@@ -4,3 +4,9 @@ Leeds XR (c) 2020
 All Rights Reserved
 */
 
+window.onload = function(){
+	//if not a https connection force a https connection
+	if (location.protocol !== 'https:') {
+		location.replace(`https:${location.href.substring(location.protocol.length)}`);
+	}
+}
